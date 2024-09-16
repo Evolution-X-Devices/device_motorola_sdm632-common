20df1e6040d0fdfc022eec2b55d2a5b99eb9823a
Deivid21 <david.parra.ignacio@gmail.com>
2024-09-16 15:58:59 -0300

    sdm632-common: Drop Moto Stuff
    Change-Id: I1e94fde9fbb6ae37fd06267d32eee88ee2d870de

--- common.mk
+++ common.mk
@@ -101,9 +101,6 @@ PRODUCT_PACKAGES += \
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
-# Dex-pre-opt exclusions
-$(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
